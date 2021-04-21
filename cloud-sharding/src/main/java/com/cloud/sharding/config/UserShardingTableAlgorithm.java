2171dfe6bb006bf9ebf3ffd0af095807b30347dc
admin <17639627374@163.com>
2021-04-21 14:30:55 +0800

    sharding 自定义配置分表策略

--- cloud-sharding/src/main/java/com/cloud/sharding/config/UserShardingTableAlgorithm.java
+++ cloud-sharding/src/main/java/com/cloud/sharding/config/UserShardingTableAlgorithm.java
@@ -6,21 +6,21 @@ import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
 import java.util.Collection;
 
 /**
- * @description:
- * @projectName:dd-cat-cloud
- * @see:com.cloud.sharding.config
+ * @description: db_user自定义分表策略
  * @author: dd-cat
- * @createTime:2021/4/20 9:56
+ * @createTime:2021/4/16 14:14
  * @version:1.0
  */
-public class UserTableAlgorithm implements PreciseShardingAlgorithm<Integer> {
+public class UserShardingTableAlgorithm implements PreciseShardingAlgorithm<Integer> {
+
     public String doSharding(Collection<String> collection, PreciseShardingValue<Integer> preciseShardingValue) {
         StringBuilder sb = new StringBuilder();
-        Integer value = preciseShardingValue.getValue();
-        //获取设置的虚拟表名称，这里获取到的logicTableName=t_order
+        //分成5张表，根据id取余5
+        Integer value = preciseShardingValue.getValue() % 5;
+        //获取设置的虚拟表名称，这里获取到的logicTableName=db_user
         String logicTableName = preciseShardingValue.getLogicTableName();
-        //拼接实际的表名称，value为carParkId字段的值
-        sb.append(logicTableName).append(value);
+        //拼接实际的表名称
+        sb.append(logicTableName).append("_").append(value);
         return sb.toString();
     }
 }
